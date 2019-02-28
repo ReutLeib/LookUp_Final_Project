3e663660bf370fe5b448d22deb706834bd29a9ff
ReutLeib <leibreut@mail.com>
2019-02-28 14:59:35 +0200

    continue with user functions

--- LookUp_Server_side/config.js
+++ LookUp_Server_side/config.js
@@ -11,8 +11,11 @@ module.exports = {
   errors: {
     ERROR_REGISTRATION: "Someting went wrong with the REGISTRATION! check your attributes and try again.",
     ERROR_ADDING: "Someting went wrong with the ADDING! check your attributes and try again.",
+    // user errors:
     ERROR_USER_EXIST: "User exist allready!",
     ERROR_USER_NOT_EXIST: "User not exist!",
+    ERROR_TITLE_NOT_EXIST: "title is not existing in database!",
+    // database errors
     ERROR_DB_CREATE_USER: "Someting went wrong with create user in database!"
   }
 
--- LookUp_Server_side/controllers/User_ctl.js
+++ LookUp_Server_side/controllers/User_ctl.js
@@ -29,7 +29,7 @@ exports.insertUser = (req, res) => {
       });
 };
 
-var isUniqueEmail = (email) => {
+var isUniqueEmail = (email) => { // return booean
 
       console.log("Enter function: isUniqueEmail");
 
@@ -55,7 +55,7 @@ var isUniqueEmail = (email) => {
       });
 };
 
-var creatUser = (_name, _email, _password, _accessibility, _disables, _birthDay, _profilePicture) => {
+var creatUser = (_name, _email, _password, _accessibility, _disables, _birthDay, _profilePicture) => { // return message
 
       console.log("Enter function: creatUser");
 
@@ -88,8 +88,68 @@ var creatUser = (_name, _email, _password, _accessibility, _disables, _birthDay,
 exports.deleteUserByEmail = (req, res) => {
 
       console.log("Enter route(DELETE): /deleteUserByEmail");
-      res.status(200).send("OK");
+      
+      deleteUser(req.params.title).then((result,error) => {
+            console.log("userName: " + req.params.userName);
+              if(result){
+                // if result TRUE:  
+                console.log(`RESULT: ${result}`);
+                console.log(`The user ${req.params.userName} have been deleted.`)
+                res.status(200).send(result);      
+              }
+              else{
+                // if result FALSE: 
+                console.log(`RESULT: ${result}`);
+                res.status(500).send(projectStrings.ERROR_USER_NOT_EXIST);
+              }
+            }, error =>{
+                res.status(500).send(error);
+              console.log(error);
+           });
 
 };
 
+var deleteUser = (_title) => {      // return booean
+  
+      console.log("Enter function: deleteUser");
+      
+      return new Promise((resolve, reject)=> {
+  
+        console.log(`_title: ${_title}`);
+    
+        User.findOne({title:_title}, (err, usr) => {
+            if (err) {
+              console.log(projectStrings.ERROR_TITLE_NOT_EXIST);
+              resolve(false);
+            }
+            else if(usr){
+    
+              //foreach loop that go on all the user subject's and remove him.
+              usr.subjects.forEach((element) =>{
+                deleteUserFromSubjectByUserName_UserSchema(_userName, element).then((result,error) => {
+                  if(result){}
+                  else{}
+              });
+            });
+                console.log(`${usr}`);
+                //deleteing the user
+                var myquery = { userName: _userName };
+                User.deleteOne(myquery, (err, obj)=> {
+                  if (err) {         
+                    console.log(err);
+                    resolve(false);
+                  }
+                  else{
+                    console.log(obj);
+                    resolve(true);
+                  }
+                });
+              }
+              else{
+                resolve(false);
+              }
+  
+            });
+        });
+    }; 
 ////////////////////////////////////////////////////////////////////////////////////
