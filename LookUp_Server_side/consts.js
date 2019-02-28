6c3d2b110d5cb5b0dd27870b716ff70a154f02ee
ReutLeib <leibreut@mail.com>
2019-02-28 15:27:39 +0200

    update consts file

--- LookUp_Server_side/consts.js
+++ LookUp_Server_side/consts.js
@@ -1,3 +1,13 @@
 module.exports = {
-  MLAB_KEY: "mongodb://db_usr:db_pass1@ds143070.mlab.com:43070/jemusic_db"
+  MLAB_KEY: "mongodb+srv://dbUser:dbPass@lookup-v2vtv.mongodb.net/test?retryWrites=true"
 }
+
+/** https://cloud.mongodb.com/v2/5c77d75555385512f883dd20#clusters
+ * 
+ * username:
+ * ronipolisanov@se.shenkar.ac.il
+ * 
+ * password:
+ * LOOKUP!1
+ * 
+ * **/
